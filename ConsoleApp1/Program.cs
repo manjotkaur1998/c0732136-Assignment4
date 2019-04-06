b86e441eee0a24038aaab921bb61529ecd75416a
visualstudio <visualstudio@122-PC07>
2019-04-05 21:33:54 -0400

    Section B

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,20 +12,21 @@ namespace ConsoleApp1
     class Program
     {
         ArrayList Beowulf;
-
         static void Main(string[] args)
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
             p.ReadTextFiles();
+            p.ReadTextFile();
 
-            Console.ReadLine();
+            Console.ReadKey();
+
+
         }
         public void Run()
         {
-            this.ReadTextFiles();
+            this.ReadTextFile();
         }
-
         public void ReadTextFiles()
         {
             using (StreamReader file = new StreamReader("U:\\Users\\732136\\Beowulf\\Beowulf.txt"))
@@ -36,20 +37,74 @@ namespace ConsoleApp1
 
                 while ((ln = file.ReadLine()) != null)
                 {
-                    Console.WriteLine(ln);
+
                     Beowulf.Add(ln);
+
                 }
+
                 file.Close();
-
                 counter = File.ReadLines("U:\\Users\\732136\\Beowulf\\Beowulf.txt").Count();
-
-                Console.WriteLine("lines in the file is :" + counter);
-
-
+                Console.WriteLine($"File has {counter} lines.");
 
             }
         }
-     
+
+        public void ReadTextFile()
+        {
+
+            var reader = new StreamReader("U:\\Users\\732136\\Beowulf\\Beowulf.txt");
+            string script = reader.ReadToEnd();
+
+            var text = script.Trim();
+            int Count = 0, index = 0;
+
+            while (index < text.Length)
+            {
+
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                Count++;
+
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            Console.WriteLine(" Number of Words are " + Count);
+
+        }
+
+
+        public int FindNumberOfBlankSpaces(string line)
+        {
+
+
+
+            int countletters = 0;
+            int countSpaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c))
+                {
+                    countletters++;
+                }
+
+                if (char.IsWhiteSpace(c))
+                {
+                    countletters++;
+                }
+            }
+            return countSpaces;
+
+        }
+
+
+
+
+
 
     }
+
 }
+
